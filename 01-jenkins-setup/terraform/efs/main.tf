8069d9d63a7d37e44a6d9bf0b77784ae863aa4be
Christopher <christopher.ogbunuzor@cloudreach.com>
2023-09-18 19:58:30 +0100

    add efs job

--- 01-jenkins-setup/terraform/efs/main.tf
+++ 01-jenkins-setup/terraform/efs/main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 module "efs_module" {
   source = "../modules/efs"
-  vpc_id     = "vpc-0a5ca4a92c2e10163"
-  subnet_ids = ["subnet-058a7514ba8adbb07", "subnet-0dbcd1ac168414927", "subnet-032f5077729435858"]
+  # vpc and subnet ids of default vpc in oregon region
+  vpc_id     = "vpc-034d81f0d908e1df2"
+  subnet_ids = ["subnet-0b57428ec73b391ba", "subnet-02411abc7bcfdb4d9", "subnet-0b13f4c7427435253"]
 }

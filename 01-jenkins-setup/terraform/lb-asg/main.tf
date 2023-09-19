be19adb5d0315b187abce8ba2e36cfc37cf263be
Christopher <christopher.ogbunuzor@cloudreach.com>
2023-09-19 16:58:15 +0100

    deploy lb-asg stack

--- 01-jenkins-setup/terraform/lb-asg/main.tf
+++ 01-jenkins-setup/terraform/lb-asg/main.tf
@@ -4,10 +4,13 @@ provider "aws" {
 
 module "lb-asg" {
   source        = "../modules/lb-asg"
-  subnets       = ["subnet-058a7514ba8adbb07", "subnet-0dbcd1ac168414927", "subnet-032f5077729435858"]
-  ami_id        = "ami-074d40b56472c5b9b"
+  subnets       = ["subnet-0b57428ec73b391ba", "subnet-02411abc7bcfdb4d9", "subnet-0b13f4c7427435253"]
+  # replace with jenkins controller AMI id or 
+  # ideally do this via github actions outputs 
+  # by specifying -var
+  ami_id        = "ami-023c4180f0a5be043" 
   instance_type = "t2.small"
-  key_name      = "techiescamp"
+  key_name      = "ta-lab-key"
   environment   = "dev"
-  vpc_id        = "vpc-0a5ca4a92c2e10163"
+  vpc_id        = "vpc-034d81f0d908e1df2"
 }
